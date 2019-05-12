8e4b0b606d9f952630fe08e8ef9f9740abfabe7a
Alexa Gibson <alexa.gibson@outlook.com>
2019-05-12 10:55:43 +0000

    installed pymongo and updated requirements.txt

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+mongo = PyMongo(app)
+
+MONGODB_URI = os.getenv("MONGO_URI")
+DBS_NAME = os.getenv("DBS_NAME")
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+    port=int(os.environ.get('PORT')),
+    debug=True)
